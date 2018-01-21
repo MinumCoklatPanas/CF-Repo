49f64d51059cf3bed6ce4068111812081fbb3af8
Muhamad Affan <akumahappanatuh@gmail.com>
2018-01-21 07:52:00 +0700

    Add solution for problem `A - Perfect Squares` Link: https://codeforces.com/contest/914/problem/A Tags: brute force, implementation, math, *900 Ref: https://codeforces.com/contest/914/submission/34395930

---
+++ codeforces/914/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool square(int x)
+{
+//	cout<<x<<"\n";
+	int akar=(int)sqrt(x);
+//	cout<<akar*akar<<"\n";
+	return (akar*akar==x);
+}
+
+int main()
+{
+	int n;
+	int a;
+	cin>>n;
+	int tab[1010];
+	for (int i=1;i<=n;i++)
+	 cin>>tab[i];
+//	for (int i=1;i<=n;i++)
+//	 cout<<tab[i]<<" ";
+//	cout<<"\n";
+	sort(tab+1,tab+n+1);
+	for (int i=n;i>=1;i--)
+	 {
+//	 	cout<<tab[i]<<"\n";
+	 	if (!square(tab[i]))
+	 	{
+	 		cout<<tab[i]<<"\n";
+	 		return 0;
+	 	}
+	 }
+}
