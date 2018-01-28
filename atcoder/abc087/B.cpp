4fd1071317de565a77753677a8021b76862fb968
Muhamad Affan <akumahappanatuh@gmail.com>
2018-01-28 19:05:00 +0700

    Add solution for problem `B - Coins` Link: https://atcoder.jp/contests/abc087/tasks/abc087_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc087/submissions/2027451

---
+++ atcoder/abc087/B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	int ans=0;
+	for (int i=0;i<=a;i++)
+	 for (int j=0;j<=b;j++)
+	  for (int k=0;k<=c;k++)
+	  {
+	  	ans+=(((i*500)+(j*100)+(k*50))==x);
+	  }
+	cout<<ans<<"\n";
+}
