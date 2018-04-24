01cd8bbd7ba0104a27197b546e5f56749930a470
Muhamad Affan <akumahappanatuh@gmail.com>
2018-04-24 19:49:00 +0700

    Add solution for problem `A - Splits` Link: https://codeforces.com/contest/964/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/964/submission/37572796

--- codeforces/964/A.cpp
+++ codeforces/964/A.cpp
@@ -5,5 +5,18 @@ int main()
 {
 	int n;
 	cin>>n;
-	cout<<(n/2)+1<<"\n";
+	int l=1;
+	int r=n;
+	while (l<r)
+	{
+		int mid=(l + r)>>1;
+		if ((n/mid)==1)
+		 r=mid;
+		else
+		 l=mid+1;
+	}
+	if (n/l==1)
+	 cout<<l<<"\n";
+	else
+	 cout<<r<<"\n";
 }
