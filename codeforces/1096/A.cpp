4e8af1a766acb9f5ae49f7f87aef7aaac34fd0f9
Muhamad Affan <akumahappanatuh@gmail.com>
2019-07-19 22:09:00 +0700

    Add solution for problem `A - Find Divisible` Link: https://codeforces.com/contest/1096/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1096/submission/57333575

---
+++ codeforces/1096/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		ll l,r;
+		cin >> l >> r;
+		cout << l << " " << (2 * l) << endl;
+	}	
+}
