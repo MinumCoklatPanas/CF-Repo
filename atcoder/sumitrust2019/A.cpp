b1b8672a82691f21e7a010b6c4da4ad5ceffd329
Muhamad Affan <akumahappanatuh@gmail.com>
2019-12-13 19:56:00 +0700

    Add solution for problem `A - November 30` Link: https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/sumitrust2019/submissions/8937168

---
+++ atcoder/sumitrust2019/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll INF = 1e9;
+const ll INFF = 1e18;
+const ll MOD = 1e9 + 7;
+const ll MAXN = 1e3;
+const double EPS = 1e-9;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int m1,d1,m2,d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cout << (m2 != m1) << endl;
+}
