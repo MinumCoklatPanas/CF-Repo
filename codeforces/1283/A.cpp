44cfe59df37984d16c683c084eb9ca76e08f1bdd
Muhamad Affan <akumahappanatuh@gmail.com>
2019-12-29 00:07:00 +0700

    Add solution for problem `A - Minutes Before the New Year` Link: https://codeforces.com/contest/1283/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1283/submission/67797706

---
+++ codeforces/1283/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int h,m;
+		cin >> h >> m;
+		int tot = 24 * 60;
+		int cur = h * 60 + m;
+		int ans = tot - cur;
+		cout << ans << endl;
+	}
+}
