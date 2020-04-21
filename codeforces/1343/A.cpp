90095c8ac6c8380bd149e67597845827d87be08a
Muhamad Affan <akumahappanatuh@gmail.com>
2020-04-21 21:43:00 +0700

    Add solution for problem `A - Candies` Link: https://codeforces.com/contest/1343/problem/A Tags: brute force, math, *900 Ref: https://codeforces.com/contest/1343/submission/77484201

---
+++ codeforces/1343/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const ll INF = 1e9;
+const ll INFF = 1e18;
+const ll MAXN = 510;
+const ll MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		ll n;
+		cin >> n;
+		ll cur = 1;
+		for (ll i = 1 ; cur <= n ; i++)
+		{
+			cur += (1ll << i);
+			if (n % cur == 0)
+			{
+				cout << n / cur << endl;
+				break;
+			} 
+		}
+	}
+	return 0;
+}
