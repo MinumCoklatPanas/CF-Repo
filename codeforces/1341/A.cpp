fb9f57f323b9c220ca05fbab88929e584eaf1478
Muhamad Affan <akumahappanatuh@gmail.com>
2020-05-01 15:01:00 +0700

    Add solution for problem `A - Nastya and Rice` Link: https://codeforces.com/contest/1341/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1341/submission/78633766

---
+++ codeforces/1341/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n,a,b,c,d;
+		cin >> n >> a >> b >> c >> d;
+		int tmp = c - d;
+		int mn = (tmp / n) + (tmp % n != 0);
+		int mx = (c + d) / n;
+		if ((mx < a - b) || (mn > a + b))
+			cout << "No\n";
+		else
+			cout << "Yes\n";
+	}
+	return 0;
+}
