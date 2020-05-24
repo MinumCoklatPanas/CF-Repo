440acbf1ee0ab6f0fc2fec278aa2ac8c2428bc16
Muhamad Affan <akumahappanatuh@gmail.com>
2020-05-24 20:44:00 +0700

    Add solution for problem `B - Honest Coach` Link: https://codeforces.com/contest/1360/problem/B Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1360/submission/81211553

---
+++ codeforces/1360/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int tab[55];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		for (int i = 1 ; i <= n ; i++)
+			cin >> tab[i];
+		sort(tab + 1,tab + n + 1);
+		int ans = INF;
+		for (int i = 1 ; i + 1 <= n ; i++)
+			ans = min(ans,tab[i + 1] - tab[i]);
+		cout << ans << endl;
+	}
+	return 0;
+}
