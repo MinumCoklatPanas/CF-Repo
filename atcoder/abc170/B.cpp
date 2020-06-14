8dcf80ee0c43ec6b3d889c8b60e839d52af682bf
Muhamad Affan <akumahappanatuh@gmail.com>
2020-06-14 19:03:00 +0700

    Add solution for problem `B - Crane and Turtle` Link: https://atcoder.jp/contests/abc170/tasks/abc170_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc170/submissions/14291102

---
+++ atcoder/abc170/B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int x,y;
+    cin >> x >> y;
+    for (int i = 0 ; i <= x ; i++)
+    {
+        if (2 * i + 4 * (x - i) == y)
+        {
+            cout << "Yes\n";
+            return 0;
+        }
+    }
+    cout << "No\n";
+    return 0;
+}
