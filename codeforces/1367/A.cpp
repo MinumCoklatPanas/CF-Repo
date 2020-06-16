f5f3ec19eaff044da4db56590f414800c8352588
Muhamad Affan <akumahappanatuh@gmail.com>
2020-06-16 21:43:00 +0700

    Add solution for problem `A - Short Substrings` Link: https://codeforces.com/contest/1367/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1367/submission/83942956

---
+++ codeforces/1367/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        string ans;
+        ans += s[0];
+        for (int i = 1 ; i < s.length() ; i += 2)
+        {
+            ans += s[i];
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
