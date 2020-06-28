403a0fa8b0995fb2af3838817ebd625765c21130
Muhamad Affan <akumahappanatuh@gmail.com>
2020-06-28 21:41:00 +0700

    Add solution for problem `B - Multiply by 2, divide by 6` Link: https://codeforces.com/contest/1374/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1374/submission/85301455

---
+++ codeforces/1374/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 3 != 0 && n != 1)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        int ans = 0;
+        while (n > 1)
+        {
+            if (n % 6 == 0)
+            {
+                ++ans;
+                n /= 6;
+            }
+            else
+            if (n % 3 == 0)
+            {
+                ans += 2;
+                n /= 3;
+            }
+            else
+                break;
+        }
+        if (n == 1)
+            cout << ans << endl;
+        else
+            cout << -1 << endl;
+    }
+    return 0;
+}
