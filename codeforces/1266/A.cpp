a5e77fa5a2cd8e2d8721b862655f48fbb7fa8b4d
Muhamad Affan <akumahappanatuh@gmail.com>
2020-08-19 13:10:00 +0700

    Add solution for problem `A - Competitive Programmer` Link: https://codeforces.com/contest/1266/problem/A Tags: chinese remainder theorem, math, *1000 Ref: https://codeforces.com/contest/1266/submission/90361858

---
+++ codeforces/1266/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    bool ada_nol = 0;
+    bool ada_genap = 0;
+    int sum = 0;
+    for (int i = 0 ; i < s.length() ; i++) 
+    {
+        if (s[i] == '0')
+        {
+            if (!ada_nol)
+                ada_nol = 1;
+            else
+                ada_genap = 1;
+        }
+        else
+        {
+            int tmp = s[i] - '0';
+            sum += tmp;
+            if (tmp % 2 == 0)
+                ada_genap = 1;
+        }
+    }
+    if (!ada_nol)
+    {
+        cout << "cyan\n";
+        return;
+    }
+    if (sum % 3 == 0 && ada_genap)
+    {
+        cout << "red\n";
+        return;
+    }
+    else
+        cout << "cyan\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
