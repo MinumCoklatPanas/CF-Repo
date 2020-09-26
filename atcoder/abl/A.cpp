55cc6b2c22fc011fe3109469ec1e1938333e56d4
Muhamad Affan <akumahappanatuh@gmail.com>
2020-09-26 19:01:00 +0700

    Add solution for problem `A - Repeat ACL` Link: https://atcoder.jp/contests/abl/tasks/abl_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abl/submissions/17021670

---
+++ atcoder/abl/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 1 ; i <= n ; i++)
+        cout << "ACL";
+    cout << endl;
+    return 0;
+}
