9b74ab9fc2217ed0cbe18b93ef54684cf064fe36
Muhamad Affan <akumahappanatuh@gmail.com>
2020-11-20 06:00:00 +0700

    Add solution for problem `E - Beautiful Subarrays` Link: https://codeforces.com/contest/665/problem/E Tags: data structures, divide and conquer, strings, trees, *2100 Ref: https://codeforces.com/contest/665/submission/98989276

---
+++ codeforces/665/E.cpp
@@ -0,0 +1,93 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct node
+{
+    node *child[2] = { NULL };
+    ll cnt = 0;
+    bool akhir = 0;
+};
+
+void insert(node *root,int k)
+{
+    node *tmp = root;
+    for (int i = 30 ; i >= 0 ; i--)
+    {
+        int ix = ((k & (1 << i)) > 0);
+        // cout << i << " " << ix << " " << k << endl;
+        if (tmp -> child[ix] == NULL)
+            tmp -> child[ix] = new node;
+        tmp = tmp -> child[ix];
+        (tmp -> cnt)++;
+    }
+    tmp -> akhir = true;
+}
+
+void dfs(node *root, int &k, int curSum, ll& ans, int depth,int x)
+{
+    if (!root) return;
+    // if (x == 3)
+    // {
+    //     cout << curSum << " " << depth << " " << (root -> cnt) << endl;
+    //     if (root -> child[0]) cout << "ada 0\n";
+    //     if (root -> child[1]) cout << "ada 1\n";
+    //     cout << endl;
+    // }
+    // cout << curSum + (1 << depth) - 1 << endl;
+    if (curSum + (1ll << depth) - 1 < k) return;
+    if (curSum >= k)
+    {
+        ans += (root -> cnt);
+        return;
+    }
+    if (!(x & (1 << (depth - 1))))
+    {
+        dfs(root -> child[0], k, curSum, ans, depth - 1, x);
+        // cout << "yo\n";
+        dfs(root -> child[1], k, curSum + (1 << (depth - 1)), ans, depth - 1, x);
+    }
+    else
+    {
+        dfs(root -> child[1], k, curSum, ans, depth - 1, x);
+        dfs(root -> child[0], k, curSum + (1 << (depth - 1)), ans, depth - 1, x);
+    }
+    return;
+}
+
+int main()
+{
+    int n,k;
+    scanf("%d %d",&n,&k);
+    // cin >> n >> k;
+    int curXor = 0;
+    node *root = new node;
+    insert(root, 0);
+    // return 0;
+    ll ans = 0;
+    for (int i = 1 ; i <= n ; i++)
+    {
+        int ta;
+        scanf("%d",&ta);
+        curXor ^= ta;
+        insert(root, curXor);
+        dfs(root, k, 0, ans, 31, curXor);
+    }
+    // cout << ans << endl;
+    printf("%lld\n",ans);
+    return 0;
+}
+
