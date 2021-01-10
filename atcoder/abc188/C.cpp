ffad6e4e56cd08bcc24390396e473907370530f6
Muhamad Affan <akumahappanatuh@gmail.com>
2021-01-10 20:30:00 +0800

    Add solution for problem `C - ABC Tournament` Link: https://atcoder.jp/contests/abc188/tasks/abc188_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc188/submissions/19335612

---
+++ atcoder/abc188/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    int mx = 0;
+    int kiri = -1;
+    for (int i = 1 ; i <= (1 << (n - 1)) ; i++)
+    {
+        int ta;
+        cin >> ta;
+        if (ta > mx)
+        {
+            mx = ta;
+            kiri = i;
+        }
+    }
+    int mxKanan = 0;
+    int kanan = -1;
+    for (int i = (1 << (n - 1)) + 1 ; i <= (1 << n) ; i++)
+    {
+        int ta;
+        cin >> ta;
+        if (ta > mxKanan)
+        {
+            mxKanan = ta;
+            kanan = i;
+        }
+    }
+    if (mx < mxKanan)
+        cout << kiri << endl;
+    else
+        cout << kanan << endl;
+    return 0;
+}
