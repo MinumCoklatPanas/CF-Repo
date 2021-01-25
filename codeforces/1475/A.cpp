6dfa2288e089f572e4f9d2f481bbc6378008d2b4
Muhamad Affan <akumahappanatuh@gmail.com>
2021-01-25 23:06:00 +0800

    Add solution for problem `A - Odd Divisor` Link: https://codeforces.com/contest/1475/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1475/submission/105318193

---
+++ codeforces/1475/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    while (n > 1)
+    {
+        if (n & 1)
+        {
+            cout << "YES\n";
+            return;
+        }
+        n /= 2;
+    }
+    cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
