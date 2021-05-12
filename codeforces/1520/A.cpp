9dcea87ffe8ea46300f272a960bc222980ef37fc
Muhamad Affan <akumahappanatuh@gmail.com>
2021-05-12 20:40:00 +0800

    Add solution for problem `A - Do Not Be Distracted!` Link: https://codeforces.com/contest/1520/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1520/submission/116025807

---
+++ codeforces/1520/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int i = 0;
+    while (i < n)
+    {
+        char awal = s[i];
+        while (s[i] == awal) ++i;
+        for (int j = i ; j < n ; j++)
+        {
+            if (s[j] == awal)
+            {
+                cout << "NO\n";
+                return;
+            }
+        }
+    }
+    cout << "YES\n";
+}
+//problem-url: https://codeforces.com/contest/1520/problem/A
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int hehe = 0;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
