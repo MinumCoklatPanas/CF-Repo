502246e17ec9a74b4023136973d59f8693d3e810
Muhamad Affan <akumahappanatuh@gmail.com>
2021-07-27 22:44:00 +0800

    Add solution for problem `D - Caesar's Legions` Link: https://codeforces.com/contest/118/problem/D Tags: dp, *1700 Ref: https://codeforces.com/contest/118/submission/123946533

---
+++ codeforces/118/D.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+#define int long long
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e8;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define MULTI_TC 0
+
+int getTC()
+{
+    if (MULTI_TC)
+    {
+        int t;
+        cin >> t;
+        return t;
+    }
+    else return 1;
+}
+
+int add(int x,int y)
+{
+    return ((x % MOD) + (y % MOD)) % MOD;
+}
+
+int sub(int x,int y)
+{
+    int ret = ((x % MOD) - (y % MOD)) % MOD;
+    if (ret < 0) ret += MOD;
+    return ret;
+}
+
+int mul(int x,int y)
+{
+    return ((x % MOD) * (y % MOD)) % MOD;
+}
+
+int modpow(int x,int y)
+{
+    if (!y) return 1;
+    int z = modpow(x,y / 2);
+    z = mul(z,z);
+    if (y & 1) z = mul(z,x);
+    return z;
+}
+
+int reverse(int x)
+{
+    return modpow(x,MOD - 2);
+}
+
+int bagi (int x,int y)
+{
+    return mul(x,reverse(y));
+}
+
+int n,m,k1,k2;
+int dp[110][110][15][15];
+
+int rec(int foot,int horse,int consecFoot,int consecHorse)
+{
+    if (foot == 0 && horse == 0) return 1;
+    if (dp[foot][horse][consecFoot][consecHorse] != -1) return dp[foot][horse][consecFoot][consecHorse];
+    int ret = 0;
+    if (foot > 0)
+    {
+        if (consecFoot < k1)
+            ret = add(ret,rec(foot - 1,horse,consecFoot + 1,0));
+    }
+    if (horse > 0)
+    {
+        if (consecHorse < k2)
+            ret = add(ret,rec(foot,horse - 1,0,consecHorse + 1));
+    }
+    return dp[foot][horse][consecFoot][consecHorse] = ret;
+}
+
+void solve()
+{
+    cin >> n >> m >> k1 >> k2;
+    memset(dp,-1,sizeof(dp));
+    int ans = rec(n,m,0,0);
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = getTC();
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
