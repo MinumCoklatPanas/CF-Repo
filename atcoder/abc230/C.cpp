a56b1b366ff8a8a3b7464e3dd24014f595b5cc16
Muhamad Affan <akumahappanatuh@gmail.com>
2021-12-03 19:13:00 +0700

    Add solution for problem `C - X drawing` Link: https://atcoder.jp/contests/abc230/tasks/abc230_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc230/submissions/27652603

---
+++ atcoder/abc230/C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+#define int long long
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define MULTI_TC 0
+
+int getTC()
+{
+    if (MULTI_TC)
+    {
+        int t;
+        cin >> t;
+        return t;
+    }
+    else return 1;
+}
+
+void solve()
+{
+    int n,a,b;
+    cin >> n >> a >> b;
+    int p,q,r,s;
+    cin >> p >> q >> r >> s;
+    for (int i = p ; i <= q ; i++)
+    {
+        for (int j = r ; j <= s ; j++)
+        {
+            int dist = abs(i - a) + abs(j - b);
+            int distRow = abs(i - a);
+            if (dist == 2 * distRow)
+                cout << "#";
+            else
+                cout << ".";
+        }
+        cout << endl;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = getTC();
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
