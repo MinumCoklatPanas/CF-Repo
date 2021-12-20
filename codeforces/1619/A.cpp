35a626ddfc51a6867c21b27dd987dba1bb34c745
Muhamad Affan <akumahappanatuh@gmail.com>
2021-12-20 21:36:00 +0700

    Add solution for problem `A - Square String?` Link: https://codeforces.com/contest/1619/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1619/submission/140017279

---
+++ codeforces/1619/A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+#define int long long
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define MULTI_TC 1
+
+int getTC()
+{
+    if (MULTI_TC)
+    {
+        int t;
+        cin >> t;
+        return t;
+    }
+    else return 1;
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.length() % 2 == 1)
+        cout << "NO\n";
+    else
+    {
+        string ss;
+        for (int i = 0 ; i < s.length() / 2 ; i++) ss += s[i];
+        ss += ss;
+        if (ss == s)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = getTC();
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
