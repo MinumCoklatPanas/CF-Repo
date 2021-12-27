3a79aa7066dda4dbbbe70033302ed496ceec6be9
Muhamad Affan <akumahappanatuh@gmail.com>
2021-12-27 22:48:00 +0700

    Add solution for problem `C - Set or Decrease` Link: https://codeforces.com/contest/1622/problem/C Tags: binary search, brute force, greedy, sortings, *1600 Ref: https://codeforces.com/contest/1622/submission/140799002

---
+++ codeforces/1622/C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Made with <3 by MinumCoklatPanas
+typedef long long ll;
+#define int long long
+const int INF = 1e9;
+const ll INFF = 1e18;
+const int MAXN = 510;
+const int MOD = 1e9 + 7;
+const double PI = acos(-1.0);
+const double INFD = 1E9;
+const double EPS = 1e-9;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define MULTI_TC 1
+
+int getTC()
+{
+    if (MULTI_TC)
+    {
+        int t;
+        cin >> t;
+        return t;
+    }
+    else return 1;
+}
+
+void solve()
+{
+    int n,k;
+    cin >> n >> k;
+    vector<int> tab(n);
+    int sum = 0;
+    for (auto &it : tab)
+    {
+        cin >> it;
+        sum += it;
+    }
+    int target = sum - k;
+    if (target <= 0)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    // if (n == 1)
+    // {
+    //     cout << tab[0] - k << endl;
+    //     return;
+    // }
+    sort(tab.begin(),tab.end());
+    const auto cei = [&](int x,int y) -> int
+    {
+        return (x / y) + (x % y != 0);
+    };
+    int ans = target;
+    int cur = 0;
+    for (int i = n - 1 ; i >= 0 ; i--)
+    {
+        int take = n - i;
+        if (i > 0)
+            cur += tab[i];
+        int atas = cur + tab[0] - target;
+        int bawah = take + (i != 0);
+        // cerr << atas << " " << bawah << endl;
+        int change;
+        if (atas < 0)
+            change = -1ll * cei(abs(atas),bawah);
+        else
+            change = atas / bawah;
+        change = min(change,tab[0]);
+        int step = tab[0] - change + (take - (i == 0));
+        // cerr << atas << " " << bawah << " " << change << " " << step << endl;
+        ans = min(ans,step);
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = getTC();
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
